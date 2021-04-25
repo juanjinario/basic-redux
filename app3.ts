0ea6abe9f19b6424101b20865b89acfe41a015e4
juanjinario <juanjco2@gmail.com>
2021-04-26 00:10:43 +0200

    Add dispatch

--- app3.ts
+++ app3.ts
@@ -1,6 +1,6 @@
 import { decrementAction, incrementAction, multiplyAction, resetAction } from "./count/count.actions";
 import { countReducer } from "./count/count.reducer";
-import { IAction, IReducer } from "./ngrx-fake/ngrx";
+import { IAction, IReducer } from './ngrx-fake/ngrx';
 
 class Store<T> {
   constructor(private reducer: IReducer<T>,
@@ -10,7 +10,14 @@ class Store<T> {
   getState() {
     return this.state;
   }
+
+  dispatch(action: IAction) {
+    this.state = this.reducer(this.state, action);
+  }
 }
 
 const store = new Store(countReducer, 10);
+console.log(store.getState());
+
+store.dispatch(incrementAction);
 console.log(store.getState());
